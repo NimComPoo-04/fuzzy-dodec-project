7426a8548ee5e60612cf52b19305d6ecca7763c1
NimComPoo-04 <bhattacharyyaaanjishnu.04@gmail.com>
2021-07-31 12:28:42 +0530

    did some testing and i don't want to loose progress

---
+++ doc/main/images.pl
@@ -0,0 +1,11 @@
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# Associate images original text with physical files.
+
+
+$key = q/hookrightarrow;MSF=1;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE=""
+ SRC="|."$dir".q|img1.svg"
+ ALT="$\hookrightarrow$">;WIDTH="0" HEIGHT="0"|; 
+
+1;
+
